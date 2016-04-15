143904ead2c1de16effbddc033585bb0d5b5ca5c
mitchellnau <mmh327@nau.edu>
2016-04-14 19:01:16 -0700

    admin changes and gitignore

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from main.models import Venue
+from main.models import Customer, Employee, ItemOrderLink, Menu, Item, Order, Venue
 
-class VenueAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'address', 'description', 'website', )
+#class VenueAdmin(admin.ModelAdmin):
+#    list_display = ('id', 'name', 'address', 'description', 'website', )
+
+admin.site.register(Customer)
+admin.site.register(Employee)
+admin.site.register(ItemOrderLink)
+admin.site.register(Menu)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(Venue)
