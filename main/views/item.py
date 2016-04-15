0789f262aa7614be6e5c757233bbbfdf5eb3c343
mitchellnau <mmh327@nau.edu>
2016-04-15 15:04:03 -0700

    item view allowing edit

--- main/views/item.py
+++ main/views/item.py
@@ -20,3 +20,21 @@ def add(request, user_id, menu_id):
     else:
         return HttpResponseRedirect('/user/home/' + str(menu.venue.user.id) + '/menu/' + str(menu.id) + '/add-item')
     return render(request, 'main/item_add.html', {'form': form})
+
+def edit(request, user_id, menu_id, item_id):
+    menu = Menu.objects.get(pk=menu_id)
+    item = Item.objects.get(pk=item_id)
+    form = AddItemForm(instance=item)
+    if request.method == 'POST':
+        form = AddItemForm(request.POST, instance=item)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.menu = menu
+            new_item.save()
+            messages.success(request, "Item successfully added")
+            return HttpResponseRedirect('/user/home/' + str(menu.venue.user.id) + '/menu/' + str(menu.id) + '/')
+    elif request.method == 'GET':
+        form = AddItemForm(instance=item)
+    else:
+        return HttpResponseRedirect('/user/home/' + str(menu.venue.user.id) + '/menu/' + str(menu.id) + '/edit-item')
+    return render(request, 'main/item_edit.html', {'form': form})
