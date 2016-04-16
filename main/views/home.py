a1fc6a54005df58c6f407a5821f33e3032a64d4f
Michael Gilbert <drumz4jah@yahoo.com>
2016-04-16 00:59:16 -0700

    adding related_name to models meant that one line in home had to change

--- main/views/home.py
+++ main/views/home.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 def home(request, user_id):
     user = User.objects.get(pk=user_id)
-    venues = user.venue_set.all()
+    venues = user.venues.all()
     menus = Menu.objects.all()
     return render(request, "main/home.html", {'user': user, 'venues': venues, 'menus': menus})
