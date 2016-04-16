e29237c43223b9613dbcbfc84dfee60d0162650e
Michael Gilbert <drumz4jah@yahoo.com>
2016-04-15 23:26:14 -0700

    added related_name for reverse-lookup of customers by venue

--- main/models/customer.py
+++ main/models/customer.py
@@ -2,8 +2,8 @@ from django.db import models
 from .venue import Venue
 
 class Customer(models.Model):
-    venue = models.ForeignKey(Venue)
-    name = models.CharField(max_length=25, default="")
+    venue = models.ForeignKey(Venue, related_name='customers')
+    name = models.CharField(max_length=25, default="", null=True, blank=True)
     number = models.IntegerField() # either table number or wristband number
 
     def __str__(self):
