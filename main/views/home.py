15c92f82c498b48e6c52da12e36255ac06160269
mitchellnau <mmh327@nau.edu>
2016-04-16 14:31:20 -0700

    passing employee objects in through home view

--- main/views/home.py
+++ main/views/home.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from main.models import Venue, Menu
+from main.models import Venue, Menu, Employee
 from django.contrib.auth.models import User
 
 def home(request, user_id):
     user = User.objects.get(pk=user_id)
     venues = user.venues.all()
     menus = Menu.objects.all()
-    return render(request, "main/home.html", {'user': user, 'venues': venues, 'menus': menus})
+    employees = Employee.objects.all()
+    return render(request, "main/home.html", {'user': user, 'venues': venues, 'menus': menus, 'employees': employees})
