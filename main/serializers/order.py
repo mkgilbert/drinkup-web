57a3df74cc2b89995baf900ab609743817272d76
Michael Gilbert <drumz4jah@yahoo.com>
2016-04-16 18:05:44 -0700

    cleaned up code, added comments

--- main/serializers/order.py
+++ main/serializers/order.py
@@ -17,17 +17,6 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Order.objects.create(**validated_data)
-        # items_data = validated_data.pop('items')
-        # print("items_data: " + items_data)
-        # #cust_id = validated_data.pop('cust_id')
-        # #print("cust_id: " + str(cust_id))
-        # #customer = Customer.objects.get(pk=cust_id)
-        # order = Order.objects.create(**validated_data)
-        # for item_data in items_data:
-        #     item_id = int(item_data['item_id'])
-        #     qty = int(item_data['item_qty'])
-        #     order.add_item(item_id, qty)
-        # return order
 
 class ItemOrderLinkSerializer(serializers.ModelSerializer):
     item = ItemSerializer(read_only=True)
@@ -37,10 +26,20 @@ class ItemOrderLinkSerializer(serializers.ModelSerializer):
         model = ItemOrderLink
         fields = ('id', 'item', 'order', 'item_order_price')
 
-# don't know if i need this
+
 class CreateOrderElement(serializers.Serializer):
+    """
+    This class is super important! I couldn't get orders to be created via the api
+    because I needed to make an order using a customer and venue object and then add
+    a bunch of items to it. Well, it turns out that the only way I could make it work is
+    by creating this funky little serializer CreateOrderElement in order to create the order
+    and add all the itmes we want to it. I had to override the __init__ method in order to
+    shove in an Order object instance that can have items added it to. So this class just
+    serializes item ids and quantities into items by adding them to the self.order instance
+    """
     item_id = serializers.IntegerField()
     item_qty = serializers.IntegerField()
+
 
     def __init__(self, *args, **kwargs):
         self.order = kwargs.pop('order')
--- main/views/api/order.py
+++ main/views/api/order.py
@@ -3,8 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from main.models import Menu, Item, Venue, Customer, Order, ItemOrderLink
-from main.serializers.menu import MenuSerializer, ItemSerializer
-from main.serializers.order import OrderSerializer, ItemOrderLinkSerializer, CreateOrderElement
+from main.serializers.order import OrderSerializer, CreateOrderElement
 
 @api_view(['GET',])
 def order_detail(request, venue_id, order_id):
@@ -41,25 +40,11 @@ def order(request, venue_id, cust_id):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         order = Order.objects.create(venue=venue, customer=customer)
-        #items = request.POST.get('items')
 
         serializer = CreateOrderElement(data=request.data, many=True, order=order)
-        #serializer = OrderSerializer(data=request.data)
         print(serializer.initial_data)
         if serializer.is_valid():
             print(serializer.validated_data)
             serializer.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_404_NOT_FOUND)
-        # items = [{"item_id": 1, "item_qty": 1}, {"item_id": 3, "item_qty": 1}]
-        # if items is None or len(items) == 0:
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
-        #
-        # for item in items:
-        #     try:
-        #         item_obj = Item.objects.get(pk=int(item['item_id']))
-        #         order.add_item(item_obj, int(item['item_qty']))
-        #     except Item.DoesNotExist:
-        #         return Response(status=status.HTTP_404_NOT_FOUND)
-        #
-        # return Response(status=status.HTTP_201_CREATED)
