4480020cc249c08475baa67070e77aa2f1938032
mitchellnau <mmh327@nau.edu>
2016-04-17 16:26:59 -0700

    updated test views

--- main/views/test.py
+++ main/views/test.py
@@ -5,8 +5,7 @@ from django.contrib.auth.decorators import login_required
 from main.models import Venue, Menu, Item
 from django.contrib.auth.models import User
 
-def display(request, user_id, venue_id):
-    user = User.objects.get(pk=user_id)
+def display(request, venue_id):
     venue = Venue.objects.get(id=venue_id)
     items = Item.objects.all()
-    return render(request, "main/test.html", {'user': user, 'venue': venue, 'items': items})
+    return render(request, "main/test.html", {'venue': venue, 'items': items})
