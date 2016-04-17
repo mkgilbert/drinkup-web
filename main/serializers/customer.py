0d7ae35d1f040b4034f0f4819568c9b425fb8e35
Michael Gilbert <drumz4jah@yahoo.com>
2016-04-16 17:57:48 -0700

    removed venue info from customer api call

--- main/serializers/customer.py
+++ main/serializers/customer.py
@@ -3,11 +3,10 @@ from main.models import Customer
 from .venue import VenueSerializer
 
 class CustomerSerializer(serializers.ModelSerializer):
-    venue = VenueSerializer(read_only=True)
 
     class Meta:
         model = Customer
-        fields = ('id', 'venue', 'name', 'number')
+        fields = ('id','name', 'number')
 
     def create(self, validated_data):
         return Customer.objects.create(**validated_data)
