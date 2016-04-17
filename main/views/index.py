eb58f7983760c6fca042319fd04453cf7b5c5be2
Michael Gilbert <drumz4jah@yahoo.com>
2016-04-17 10:19:03 -0700

    added home link to index.html

--- main/views/index.py
+++ main/views/index.py
@@ -5,4 +5,5 @@ def index(request):
         user_is_logged_in = True
     else:
         user_is_logged_in = False
-    return render(request, 'index.html', {'user_is_logged_in': user_is_logged_in})
+    return render(request, 'index.html', { 'user': request.user,
+                                           'user_is_logged_in': user_is_logged_in} )
