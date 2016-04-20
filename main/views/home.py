65bae2511448f4d3668da3279725875659fc94c9
Grant570 <gs339@nau.edu>
2016-04-20 10:53:51 -0700

    Updated home for menus and employees. Changed home view to return items.

--- main/views/home.py
+++ main/views/home.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from main.models import Venue, Menu, Employee
+from main.models import Venue, Menu, Employee, Item
 
 @login_required()
 def home(request):
@@ -8,8 +8,10 @@ def home(request):
     venues = user.venues.all()
     menus = Menu.objects.all()
     employees = Employee.objects.all()
+    items = Item.objects.all()
     return render(request, "main/home.html", {'user': user,
                                               'venue': "",
                                               'venues': venues,
                                               'menus': menus,
+                                              'items':items,
                                               'employees': employees})
