5258136af852e8e45d26bb9dcc73fcdc300471fb
Grant570 <gs339@nau.edu>
2016-04-22 17:42:59 -0700

    Moved navbar to always be at top. Changed save and update buttons using bootstrap. Changed the homepage Menus format. Updated the edit menu page to display in table.

--- main/views/home.py
+++ main/views/home.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.template.defaulttags import register
 from main.models import Venue, Menu, Employee, Item
 
 @login_required()
@@ -9,9 +10,18 @@ def home(request):
     menus = Menu.objects.all()
     employees = Employee.objects.all()
     items = Item.objects.all()
+    cost = {}
+    for item in items:
+        cost[item.id] = ("{:,.2f}".format(item.price))
     return render(request, "main/home.html", {'user': user,
                                               'venue': "",
                                               'venues': venues,
                                               'menus': menus,
                                               'items':items,
-                                              'employees': employees})
+                                              'employees': employees,
+                                              "cost":cost})
+#ability to look up dictionary values
+#used for looking up price
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
