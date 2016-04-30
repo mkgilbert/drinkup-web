2f73aba3157216f05395b5406df9ec0d525427ad
mitchellnau <mmh327@nau.edu>
2016-04-30 14:31:16 -0700

    updated order page views

--- main/views/test.py
+++ main/views/test.py
@@ -2,10 +2,11 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from main.models import Venue, Menu, Item
+from main.models import Venue, Menu, Item, Employee
 from django.contrib.auth.models import User
 
-def display(request, venue_id):
+def display(request, venue_id, employee_id):
     venue = Venue.objects.get(id=venue_id)
     items = Item.objects.all()
-    return render(request, "main/test.html", {'venue': venue, 'items': items})
+    employee = Employee.objects.get(id=employee_id)
+    return render(request, "main/test.html", {'venue': venue, 'items': items, 'employee': employee})
